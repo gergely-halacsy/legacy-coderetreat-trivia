cf99ac59234a4d586d88f09c61d93e2ece8e0273
Gergely Halacsy <ghalacsy@diligent.com>
2024-12-02 16:13:30 +0100

    feat: make i18n type safe

--- python3/i18n.py
+++ python3/i18n.py
@@ -1,34 +1,33 @@
-from typing import Literal
+from typing import Dict, Literal
 
 
+TRANSLATABLE_TEXTS = Literal["Pop Question", "Science Question", "Sports Question"]
+LANGUAGES = Literal["en", "de", "hu", "zh"]
 
-TRANSLATABLE_TEXTS = Literal['Pop Question', 'Science Question', 'Sports Question']
-
-TRANSLATIONS = {
-    'de': {
-        'Pop Question': 'Pop Frage',
-        'Science Question': 'Science Frage',
-        'Sports Question': 'Sport Frage',
-
+TRANSLATIONS: Dict[LANGUAGES, Dict[TRANSLATABLE_TEXTS, str]] = {
+    "de": {
+        "Pop Question": "Pop Frage",
+        "Science Question": "Science Frage",
+        "Sports Question": "Sport Frage",
     },
-    'hu': {
-        'Pop Question': 'Pop kérdés',
-        'Science Question': 'Tudomány kérdés',
-        'Sports Question': 'Sport kérdés',
+    "hu": {
+        "Pop Question": "Pop kérdés",
+        "Science Question": "Tudomány kérdés",
+        "Sports Question": "Sport kérdés",
     },
-    'ch': {
-        'Pop Question': 'Pop 问题',
-        'Science Question': '科学问题',
-        'Sports Question': '体育问题',
-    }
+    "zh": {
+        "Pop Question": "Pop 问题",
+        "Science Question": "科学问题",
+        "Sports Question": "体育问题",
+    },
 }
-LANGUAGES = Literal['en', 'de', 'hu' ]
+
 
 class I18n:
-    def __init__(self, lang:LANGUAGES='en'):
+    def __init__(self, lang: LANGUAGES = "en"):
         self.lang = lang
 
-    def t(self, text:TRANSLATABLE_TEXTS):
-        if self.lang == 'en':
+    def t(self, text: TRANSLATABLE_TEXTS):
+        if self.lang == "en":
             return text
-        return TRANSLATIONS[self.lang][text] 
+        return TRANSLATIONS[self.lang][text]
