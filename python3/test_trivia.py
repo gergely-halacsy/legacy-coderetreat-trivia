188156067b43b77ecb82bf5e912aad06779adb0e
Benjamin Zvolenszki <bzvolenszki@diligent.com>
2024-12-02 10:56:29 +0100

    feat: add tests

--- python3/test_trivia.py
+++ python3/test_trivia.py
@@ -17,8 +17,35 @@ def redirect_stdout():
         sys.stdout = sys.__stdout__
 
 
-def test_dummy():
+def test_adding_player():
     with redirect_stdout() as output:
         game = Game()
         game.add('Chet')
         assert output.getvalue() == 'Chet was added\nThey are player number 1\n'
+
+
+def test_create_rock_question():
+    game = Game()
+    created_question = game.create_rock_question(42)
+    assert created_question == "Rock Question 42"
+
+
+def test_is_playable_false():
+    game = Game()
+    game.add('Chet')
+    assert game.is_playable() == False
+
+
+def test_is_playable_true():
+    game = Game()
+    game.add('Chet')
+    game.add('Pat')
+    assert game.is_playable() == True
+
+
+def test_roll():
+    with redirect_stdout() as output:
+        game = Game()
+        game.add('Chet')
+        game.add('Pat')
+        game.add('Sue')
